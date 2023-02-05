b9164270cb8752253568e73dad2a5ed1084e9c59
Dineshchandra47 <dineshchandraindia5@gmail.com.com>
2023-02-05 22:36:36 +0530

    Final commit of Product details App

---
+++ src/assets/components/Header/Header.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+export const Header = () => {
+  return (
+    <>
+      <div className="div-a">
+        <div className="container">
+          <div className="header">
+            WISHLIST
+          </div>
+          <div className="heart"></div>
+          <div className="heart-icon"></div>
+          <div className="div-line-1"></div>
+          <div className="cart">CART</div>
+          <div className="cart-icon"></div>
+          <div className="div-line-2"></div>
+          <div className="user"></div>
+          <div className="name">SAIRAM KEESARA</div>
+          <div className="arrow"></div>
+        </div>
+      </div>
+      <div className="div2">KRAYA</div>
+      <div className="phone">PHONE</div>
+      <div className="tele">TELEVISION</div>
+      <div className="refri">REFRIGERATOR</div>
+      <div className="laptop">LAPTOP</div>
+      <div className="wash">WASHING MACHINE </div>
+      <div className="down">DOWNLOADS </div>
+      <div className="search_box"></div>
+      <div className="search"></div>
+      <div className="search_">Search for products </div>
+      <div className="search_cancel"></div>
+      <div className="home-mobiles">HOME / MOBILES</div>
+      <div className="image-1"></div>
+      <div className="image-2"></div>
+      <div className="image-6"></div>
+      <div className="image-4"></div>
+      <div className="image-5"></div>
+      <div className="image-3"></div>
+      <div className="image-7"></div>
+      <div className="details">
+        Image courtesy of flipkart, www.flipkart.com
+      </div>
+      <div className="add_to_cart"></div>
+      <div className="nameofcart">Add to cart</div>
+      <div className="buynow"></div>
+      <div className="buynow-1">Buy now</div>
+
+      <div className="heart-2">
+        <div className="heart-3"></div>
+      </div>
+      <div className="container-theme"></div>
+      <div className="product_code">Product Code</div>
+      <div className="product_code-1">0DE349879</div>
+      <div className="product">
+        <h2> Honor 8 Pro (Midnight Black, 128GB) (6GB RAM)</h2>
+      </div>
+      <div className="stars"></div>
+      <div className="stars-1"></div>
+      <div className="stars-2"></div>
+      <div className="stars-3"></div>
+      <div className="stars-4"></div>
+      <div className="rating">(9,303)</div>
+      <div className="price"><h1>$99.99</h1></div>
+      <div className="strike-price"><s>$149.99</s></div>
+      <div className="disc-price"><h4>30% off</h4></div>
+      <div className="in-stock"><h3>In stock</h3></div>
+      <div className="product-color"><h5>Color</h5></div>
+      <div className="product-color-1"><h5>Sapphire Blue</h5></div>
+      <div className="product-mobile-1"></div>
+      <div className="product-mobile-2"></div>
+      <div className="product-mobile-3"></div>
+      <div className="storage-name">Storage  <b>32GB</b></div>
+      <div className="ram-name">RAM  <b>4GB</b></div>
+      <div className="storage-capacity"></div>
+      <div className="storage-capacity-1">64GB</div>
+      <div className="storage-capacity-2"></div>
+      <div className="storage-capacity-3">128GB</div>
+      <div className="ram-capacity"></div>
+      <div className="ram-capacity-1">4GB</div>
+      <div className="ram-capacity-2"></div>
+      <div className="ram-capacity-3">4GB</div>
+      <div className="product-details">+  &nbsp;  PRODUCT DETAILS</div>
+
+    </>
+  );
+};
